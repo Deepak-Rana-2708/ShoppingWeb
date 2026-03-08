c7629d4bf2c9bed4a2e99563a50949cff6237c8a
Your-dream-0812 <deepakranasandeep@gmail.com>
2026-03-08 23:56:47 +0530

    fix email issue

--- Backend/Controllers/UserController.js
+++ Backend/Controllers/UserController.js
@@ -141,15 +141,12 @@ export const forgotPassword = async (req, res) => {
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true, // true for 465
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000
+  }
 });
       
     const mail = await transporter.sendMail({
