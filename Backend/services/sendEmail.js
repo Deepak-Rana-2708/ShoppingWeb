1835bc1e00276afe06a89c4cc2d1c77f9801709b
Your-dream-0812 <deepakranasandeep@gmail.com>
2026-03-09 11:46:01 +0530

    add timeout

--- Backend/services/sendEmail.js
+++ Backend/services/sendEmail.js
@@ -11,6 +11,7 @@ export const emailSent = async (email, otp, fname) => {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
         },
+      connectionTimeout: 10000
     });
 
     const mail = await transporter.sendMail({
